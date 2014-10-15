6a9f626fe4415ff0fe8c2fe0073ee30bf33b77e5
Gabriel D <gabriel.dinizo@hotmail.com>
2014-10-15 17:22:02 -0300

    Adicionadas anotações no modelo Usuário e testes iniciais do modelo estão completos.
    Signed-off-by: Gabriel D <gabriel.dinizo@hotmail.com>

--- gameshunter/src/main/java/br/com/gameshunter/model/Usuario.java
+++ gameshunter/src/main/java/br/com/gameshunter/model/Usuario.java
@@ -2,86 +2,247 @@ package br.com.gameshunter.model;
 
 import java.util.Calendar;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+/**
+ * A classe {@code Usuario} representa os usuários do sistema.
+ * 
+ * @author Myho
+ * 
+ * @since 0.0.1
+ */
 @Entity
 public class Usuario {
 
+	/**
+	 * Valor usado para armazenar o email do usuário. {@code email} deve ser a
+	 * chave principal no banco de dados.
+	 */
 	@Id
 	private String email;
-	private String apelido;
+
+	/** Valor usado para armazenar o nome completo do usuário. */
 	private String nome;
+
+	/** Valor usado para armazenar o apelido usuário. */
+	private String apelido;
+
+	/**
+	 * Valor usado para armazenar o cpf do usuário . {@code cpf} deve ser um
+	 * valor único no banco de dados.
+	 */
+	@Column(unique = true)
+	private String cpf;
+
+	/** Valor usado para armazenar a data de nascimento do usuário */
 	@Temporal(TemporalType.DATE)
 	private Calendar dataNascimento;
-	private String endereço;
+
+	/** Valor usado para armazenar o endereço do usuário */
+	private String endereco;
+
+	/** Valor usado para armazenar o telefone do usuário */
 	private String telefone;
-	private String cpf;
+
+	/** Valor usado para armazenar o rf do usuário */
 	private String rg;
 
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
+	/**
+	 * Define o e-mail do usuário.
+	 * 
+	 * @param email
+	 *            O e-mail que deve ser definido para o usuário
+	 * @return O próprio {@code objeto}
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getApelido() {
-		return apelido;
-	}
-
-	public void setApelido(String apelido) {
-		this.apelido = apelido;
+		return this;
 	}
 
 	public String getNome() {
 		return nome;
 	}
 
-	public void setNome(String nome) {
+	/**
+	 * Define o nome do usuário.
+	 * 
+	 * @param nome
+	 * 
+	 *            O nome que deve ser definido para o usuário
+	 * @return O próprio {@code objeto}
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario setNome(String nome) {
 		this.nome = nome;
+		return this;
 	}
 
-	public Calendar getDataNascimento() {
-		return dataNascimento;
+	public String getApelido() {
+		return apelido;
 	}
 
-	public void setDataNascimento(Calendar dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getEndereço() {
-		return endereço;
-	}
-
-	public void setEndereço(String endereço) {
-		this.endereço = endereço;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
+	/**
+	 * Define o apelido do usuário.
+	 * 
+	 * @param apelido
+	 *            O apelido que deve ser definido para o usuário
+	 * @return O próprio {@code objeto}
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario setApelido(String apelido) {
+		this.apelido = apelido;
+		return this;
 	}
 
 	public String getCpf() {
 		return cpf;
 	}
 
-	public void setCpf(String cpf) {
+	/**
+	 * Define o CPF do usuário.
+	 * 
+	 * @param cpf
+	 *            O cpf que deve ser definido para o usuário
+	 * @return O próprio {@code objeto}
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario setCpf(String cpf) {
 		this.cpf = cpf;
+		return this;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	/**
+	 * Define a data de nascimento do usuário.
+	 * 
+	 * @param dataNascimento
+	 *            A data de nascimento que deve ser definido para o usuário
+	 * @return O próprio {@code objeto}
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+		return this;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	/**
+	 * Define o endereço do usuário.
+	 * 
+	 * @param endereco
+	 *            O endereço que deve ser definido para o usuário
+	 * @return O próprio {@code objeto}
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario setEndereco(String endereco) {
+		this.endereco = endereco;
+		return this;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	/**
+	 * Define o telefone do usuário.
+	 * 
+	 * @param telefone
+	 *            O telefone que deve ser definido para o usuário
+	 * @return O próprio {@code objeto}
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario setTelefone(String telefone) {
+		this.telefone = telefone;
+		return this;
 	}
 
 	public String getRg() {
 		return rg;
 	}
 
-	public void setRg(String rg) {
+	/**
+	 * Define o RG do usuário.
+	 * 
+	 * @param rg
+	 *            O RG que deve ser definido para o usuário
+	 * @return O próprio {@code objeto}
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario setRg(String rg) {
 		this.rg = rg;
+		return this;
+	}
+
+	/**
+	 * Constructor que deve ser utilizado somente pelas API's do projeto.
+	 *
+	 * @since 0.0.1
+	 */
+	@Deprecated
+	public Usuario() {
+	}
+
+	/**
+	 * @param nome
+	 *            Nome que deve ser definido para o usuário.
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario(String nome) {
+
+		this.nome = nome;
+	}
+
+	/**
+	 * @param email
+	 *            Email que deve ser definido para o usuário.
+	 * @param nome
+	 *            Nome que deve ser definido para o usuário.
+	 * @param apelido
+	 *            Apelido que deve ser definido para o usuário.
+	 * @param cpf
+	 *            CPF que deve ser definido para o usuário.
+	 * @param dataNascimento
+	 *            Data de nascimento que deve ser definida para o usuário.
+	 * @param endereco
+	 *            Endereço que deve ser definido para o usuário.
+	 * @param telefone
+	 *            Telefone que deve ser definido para o usuário.
+	 * 
+	 * @since 0.0.1
+	 */
+	public Usuario(String email, String nome, String apelido, String cpf,
+			Calendar dataNascimento, String endereco, String telefone) {
+
+		this.email = email;
+		this.nome = nome;
+		this.apelido = apelido;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+		this.endereco = endereco;
+		this.telefone = telefone;
 	}
 }
---
+++ gameshunter/src/test/java/br/com/gameshunter/model/UsuarioTest.java
@@ -0,0 +1,79 @@
+package br.com.gameshunter.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import java.util.Calendar;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UsuarioTest {
+
+	private Usuario joao;
+	private Calendar c;
+
+	@Before
+	public void criaUsuarios() {
+
+		c = Calendar.getInstance();
+		c.set(1990, 1, 12);
+		joao = new Usuario("João da Silva Machado").setEmail("joaomachado@silva.com")
+				.setApelido("Pikachu Iluminado").setCpf("000.000.000-00")
+				.setDataNascimento(c)
+				.setEndereco("Rua Vergueiro, 18, São Paulo, SP")
+				.setTelefone("(11) 1111-1111").setRg("1234567-890");
+	}
+
+	@Test
+	public void deveRetornarEmailCorreto() {
+
+		assertEquals("joaomachado@silva.com", joao.getEmail());
+	}
+
+	@Test
+	public void deveRetornarNomeCorreto() {
+
+		assertEquals("João da Silva Machado", joao.getNome());
+	}
+
+	@Test
+	public void deveRetornarApelidoCorreto() {
+
+		assertEquals("Pikachu Iluminado", joao.getApelido());
+	}
+
+	@Test
+	public void deveRetornarCpfCorreto() {
+
+		assertEquals("000.000.000-00", joao.getCpf());
+	}
+
+	@Test
+	public void deveRetornarDataCorreta() {
+
+		assertThat(joao.getDataNascimento().getTime().toString(),
+				containsString("Mon Feb 12"));
+		assertThat(joao.getDataNascimento().getTime().toString(),
+				containsString("1990"));
+	}
+
+	@Test
+	public void deveRetornarEnderecoCorreto() {
+
+		assertEquals("Rua Vergueiro, 18, São Paulo, SP", joao.getEndereco());
+	}
+
+	@Test
+	public void deveRetornarTelefoneCorreto() {
+
+		assertEquals("(11) 1111-1111", joao.getTelefone());
+	}
+
+	@Test
+	public void deveRetornarRgCorrreto() {
+
+		assertEquals("1234567-890", joao.getRg());
+	}
+}
