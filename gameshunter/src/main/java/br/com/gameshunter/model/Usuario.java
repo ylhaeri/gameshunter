f13cc788123ab2f49a8b4823ffb6332b10ffc70b
Gabriel D <gabriel.dinizo@hotmail.com>
2014-10-15 02:35:32 -0300

    Esboço da classe Usuário adicionado
    Signed-off-by: Gabriel D <gabriel.dinizo@hotmail.com>

---
+++ gameshunter/src/main/java/br/com/gameshunter/model/Usuario.java
@@ -0,0 +1,87 @@
+package br.com.gameshunter.model;
+
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Usuario {
+
+	@Id
+	private String email;
+	private String apelido;
+	private String nome;
+	@Temporal(TemporalType.DATE)
+	private Calendar dataNascimento;
+	private String endereço;
+	private String telefone;
+	private String cpf;
+	private String rg;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getEndereço() {
+		return endereço;
+	}
+
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+}
