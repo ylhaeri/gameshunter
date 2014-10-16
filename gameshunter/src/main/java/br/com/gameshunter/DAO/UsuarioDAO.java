fd38f9428e3f30ebbe18c5ca43c82ff91ccba91e
Swanter <alexfelipevieira@gmail.com>
2014-10-16 09:07:45 -0300

    Nova atualização: 	*Criação do pacote br.com.gameshunter.DAO 		*Criação da classe UsuárioDAO

---
+++ gameshunter/src/main/java/br/com/gameshunter/DAO/UsuarioDAO.java
@@ -0,0 +1,40 @@
+package br.com.gameshunter.DAO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.gameshunter.model.Usuario;
+
+/**
+ * A classe {@code UsuarioDAO} representa todo tipo de conexão relacionado à classe {@code Usuario}
+ * 
+ * @author Alex
+ *
+ * @since 0.0.1	
+ */
+
+public class UsuarioDAO {
+
+	/**
+	 * Cadastra um usuário no banco de dados
+	 * 
+	 * @param Usuário
+	 * 	 * 
+	 * @since 0.0.1
+	 */
+	
+	public void setUsuario (Usuario usuario){
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("gameshunter");
+		
+		EntityManager manager = factory.createEntityManager();
+		
+		manager.getTransaction().begin();
+		manager.persist(usuario);
+		manager.getTransaction().commit();
+		
+		manager.close();
+	}
+	
+}
