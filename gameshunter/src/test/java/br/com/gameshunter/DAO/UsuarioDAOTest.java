ffae59da4a4b3dfabd21d526e5e31b0014825b22
Swanter <alexfelipevieira@gmail.com>
2014-10-17 08:03:45 -0300

    teste

---
+++ gameshunter/src/test/java/br/com/gameshunter/DAO/UsuarioDAOTest.java
@@ -0,0 +1,14 @@
+package br.com.gameshunter.DAO;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsuarioDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
