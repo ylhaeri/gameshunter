4616ab5eefc2c103b09b8ea4f0d94943faf23df1
Swanter <alexfelipevieira@gmail.com>
2014-10-23 11:35:31 -0200

    criação dos models: Pais, Estado e Cidade

---
+++ gameshunter/src/main/java/br/com/gameshunter/model/Estado.java
@@ -0,0 +1,49 @@
+package br.com.gameshunter.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Estado {
+
+	@Id
+	private Integer id;
+	private String nome;
+	private String uf;
+	@ManyToOne
+	private Pais pais;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+
+}
