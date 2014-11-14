82535622c8201a11ed08a7591e2f73a119ea6380
Gabriel D <gabriel.dinizo@hotmail.com>
2014-11-14 14:24:18 -0200

    Aaaar

--- gameshunter/src/main/java/br/com/gameshunter/model/Usuario.java
+++ gameshunter/src/main/java/br/com/gameshunter/model/Usuario.java
@@ -30,7 +30,7 @@ public class Usuario {
 	private String email;
 	private String nome;
 	private String apelido;
-	private String senhaaaawe;
+	private String senha;
 	@Enumerated(EnumType.STRING)
 	private Sexo sexo;
 	private String cpf;
@@ -127,13 +127,13 @@ public class Usuario {
 
 	/** @return A senha */
 	public String getSenha() {
-		return senhaaaawe;
+		return senha;
 	}
 
 	/** @param senha */
 	@RequiredStringValidator(key = "usuario.senha.vazia")
 	public void setSenha(String senha) {
-		this.senhaaaawe = senha;
+		this.senha = senha;
 	}
 
 	/**
@@ -203,7 +203,7 @@ public class Usuario {
 
 	/** Gera o Hash da senha do usuário. */
 	public void geraHashDeSenha() {
-		this.senhaaaawe = new HashFactory().geraHashedString(this.senhaaaawe);
+		this.senha = new HashFactory().geraHashedString(this.senha);
 	}
 
 	/** Construtor padrão */
@@ -228,7 +228,7 @@ public class Usuario {
 		this.email = email;
 		this.nome = nome;
 		this.apelido = apelido;
-		this.senhaaaawe = senha;
+		this.senha = senha;
 		this.sexo = sexo;
 		this.cpf = cpf;
 		this.dataNascimento = dataNascimento;
@@ -248,8 +248,7 @@ public class Usuario {
 		result = prime * result
 				+ ((enderecos == null) ? 0 : enderecos.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		result = prime * result
-				+ ((senhaaaawe == null) ? 0 : senhaaaawe.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
 		result = prime * result + ((sexo == null) ? 0 : sexo.hashCode());
 		result = prime * result
 				+ ((telefone == null) ? 0 : telefone.hashCode());
@@ -295,10 +294,10 @@ public class Usuario {
 				return false;
 		} else if (!nome.equals(other.nome))
 			return false;
-		if (senhaaaawe == null) {
-			if (other.senhaaaawe != null)
+		if (senha == null) {
+			if (other.senha != null)
 				return false;
-		} else if (!senhaaaawe.equals(other.senhaaaawe))
+		} else if (!senha.equals(other.senha))
 			return false;
 		if (sexo != other.sexo)
 			return false;
