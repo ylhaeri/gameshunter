4602b33a3b1d24102a4b7413f67ab8c2edfbb294
Gabriel D <gabriel.dinizo@hotmail.com>
2015-02-06 18:43:51 -0200

    Agora funciona

--- gameshunter/src/main/java/br/com/gameshunter/controller/EnderecoController.java
+++ gameshunter/src/main/java/br/com/gameshunter/controller/EnderecoController.java
@@ -1,5 +1,7 @@
 package br.com.gameshunter.controller;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.gameshunter.model.Cep;
 
 @Controller
 @RequestMapping("endereco")
@@ -16,24 +20,16 @@ public class EnderecoController {
 	private EntityManager manager;
 
 	@RequestMapping(value = "cep/{cep}")
-	@ResponseBody
-	public aa cep(@PathVariable("cep") String cep) {
+	public @ResponseBody Cep cep(@PathVariable("cep") String cep) {
 
-		aa coisa = new aa();
-		System.out.println(coisa.getCep());
-		return coisa;
-		// List<Cep> logradouro =
-		// manager.createQuery("select l from Logradouro l where l.cep = :cep",
-		// Cep.class)
-		// .setParameter("cep", cep).getResultList();
-		// if (!logradouro.isEmpty()) {
-		// System.out.println(logradouro.get(0));
-		// return logradouro.get(0);
-		// }
-		// List<Cep> cidade =
-		// manager.createQuery("select c from Cidade c where c.cep = :cep",
-		// Cep.class)
-		// .setParameter("cep", cep).getResultList();
-		// return !cidade.isEmpty() ? cidade.get(0) : null;
+		List<Cep> logradouro = manager.createQuery("select l from Logradouro l where l.cep = :cep", Cep.class)
+				.setParameter("cep", cep).getResultList();
+		if (!logradouro.isEmpty()) {
+			System.out.println(logradouro.get(0));
+			return logradouro.get(0);
+		}
+		List<Cep> cidade = manager.createQuery("select c from Cidade c where c.cep = :cep", Cep.class)
+				.setParameter("cep", cep).getResultList();
+		return !cidade.isEmpty() ? cidade.get(0) : null;
 	}
 }
