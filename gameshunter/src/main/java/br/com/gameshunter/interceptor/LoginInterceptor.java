3e88e6e33575f5b2f812111de7926ee186545e34
Myho <Myho@ElPsyCongroo>
2015-03-16 08:51:33 -0300

    Ok

---
+++ gameshunter/src/main/java/br/com/gameshunter/interceptor/LoginInterceptor.java
@@ -0,0 +1,16 @@
+package br.com.gameshunter.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+
+		return true;
+	}
+}
