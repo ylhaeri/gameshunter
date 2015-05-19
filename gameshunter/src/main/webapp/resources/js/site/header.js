4e04b770a865ccf4dac3de4fc88523796b22130e
Swanter <alexfelipevieira@gmail.com>
2015-05-19 17:10:18 -0300

    veja

--- gameshunter/src/main/webapp/resources/js/site/header.js
+++ gameshunter/src/main/webapp/resources/js/site/header.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
 	$(".path").val(window.location.pathname.replace("/gameshunter", ""));
 
 	$('.menu-img').click(function() {
-		$('.nav').addClass('open-menu');
+		$('nav').addClass('open-menu');
 		$("html").addClass("transparent-background");
 	});
 	
 	$('.close-menu').click(function(){
 		$("html").removeClass("transparent-background");
-		$('.nav').removeClass("open-menu");
+		$('nav').removeClass("open-menu");
 	});
 	
 	
 	$(document).click(function(event) {
 		if (event.target === document.documentElement) {
 			$("html").removeClass("transparent-background");
-			$('.nav').removeClass("open-menu");
+			$('nav').removeClass("open-menu");
 		}
 
 	});
